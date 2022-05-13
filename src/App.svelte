589b8b6e1773dc701e519616ded9fe613b1fa326
Daniel <contact@jiftoo.dev>
2022-05-13 15:50:38 +0300

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+	let line1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nihil facilis dolores ipsam blanditiis dicta.";
+	let line2 = "Dolorem repellendus impedit aliquam!";
+
+	let userInput = "Your piece of the story!";
+	const maxCharacters = 180;
+	$: counterColor = userInput.length > maxCharacters * 0.9 ? "red" : "unset";
+</script>
+
+<main>
+	<center>
+		<h1>Storytelling experiment</h1>
+		<p>TODO: add description</p>
+		<br />
+		<form>
+			<blockquote>
+				<cite>{line2}</cite>
+				<cite>{line1}</cite>
+				<div>
+					<textarea type="text" bind:value={userInput} />
+					<div style="color: {counterColor}">{userInput.length} / {maxCharacters}</div>
+				</div>
+			</blockquote>
+		</form>
+	</center>
+</main>
+
+<style>
+	* {
+		font-family: "Open Sans";
+		box-sizing: border-box;
+	}
+	blockquote {
+		background-color: whitesmoke;
+
+		--padding: 24px;
+		padding: var(--padding);
+
+		max-width: 60%;
+		text-align: left;
+	}
+	blockquote *:not(:first-child) {
+		margin-top: var(--padding);
+		font-size: 16px;
+		display: block;
+	}
+	textarea {
+		border: none;
+		background-color: inherit;
+		filter: brightness(98%);
+		font-family: "Open Sans";
+		width: fit-content;
+
+		width: 100%;
+
+		--lh: 1.25;
+		line-height: var(--lh);
+		height: calc(var(--lh) * 3rem);
+	}
+	cite,
+	textarea {
+		resize: none;
+		max-width: 100%;
+		border-left: 8px solid gainsboro;
+		padding-left: 8px;
+	}
+</style>

17f4517a3d52f2a1ce8b0412256e1938a6405167
Dan Kov <0x666c@mail.ru>
2022-05-14 19:53:05 +0300

    near finished ig

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,90 @@
-import express from "express";
-import cors from "cors";
+"use strict";
 
-const app = express();
+import {WebSocket, WebSocketServer} from "ws";
+import {gib_dect} from "./gib_detect.cjs";
 
-app.use(cors());
+const wss = new WebSocketServer({port: 80});
 
-app.get("/get", (req, resp) => {
-    const episodes = ["Lorem ipsum dolor sit amet con sectetur adipisicing elit. Maiores nihil facilis dolores ipsam blanditiis dicta.", "Dolorem repellendus impedit aliquam!"];
-    resp.send(JSON.stringify(episodes));
+const episodes = [
+	"A bright flash of light strikes your eyes. As you squint and bring your hand to eyes, a figure appears from beyond the glare.",
+];
+
+let isBooked = null;
+let bookedTimer = null;
+let bookedUntil = null;
+
+const validateCoherency = (string) => {
+	return gib_dect(string);
+};
+
+const validateCharacters = (string) => {
+	return string.replaceAll(/[^A-Za-z0-9.,!?":\- ]/g, "") === string;
+};
+
+const wsMsg = (type, data) => {
+	return JSON.stringify({type, data, isBooked: !!isBooked});
+};
+const wsErr = (err) => {
+	return wsMsg("error", err);
+};
+
+const broadcast = (msg, exclude) => {
+	wss.clients.forEach((c) => {
+		if (c.readyState === WebSocket.OPEN && c !== exclude) {
+			c.send(msg);
+		}
+	});
+};
+
+wss.on("connection", (ws) => {
+	ws.on("message", (ev) => {
+		const msg = JSON.parse(ev.toString());
+		switch (msg.type) {
+			case "get":
+				ws.send(
+					wsMsg(
+						"update",
+						{bookedForMe: msg.bookId === isBooked && msg.bookId != null, episodes, bookedFor: bookedUntil === null ? 0 : bookedUntil - Date.now()},
+						msg.bookId
+					)
+				);
+				break;
+			case "submit":
+				if (!validateCharacters(msg.data) || !validateCoherency(msg.data)) {
+					ws.send(wsErr("gibberish"));
+					return;
+				}
+				if (isBooked && msg.bookId !== isBooked) {
+					ws.send(wsErr("booked"));
+					return;
+				}
+				episodes.push(msg.data.trim());
+				broadcast(
+					wsMsg(
+						"update",
+						{bookedForMe: msg.bookId === isBooked && msg.bookId != null, episodes, bookedFor: bookedUntil === null ? 0 : bookedUntil - Date.now()},
+						msg.bookId
+					)
+				);
+				break;
+			case "book":
+				if (isBooked) {
+					ws.send(wsErr("booked"));
+					return;
+				}
+				const BOOK_DURATION = 1000 * 60 * 2; // 2 minutes
+				bookedUntil = Date.now() + BOOK_DURATION;
+
+				isBooked = Math.random().toString();
+				ws.send(wsMsg("bookSuccess", isBooked));
+				broadcast(wsMsg("booked", {bookedFor: BOOK_DURATION}));
+				bookedTimer = setTimeout(() => {
+					isBooked = false;
+					bookedTimer = null;
+					bookedUntil = null;
+					broadcast(wsMsg("unbooked", episodes, null));
+					console.log("no longer booked");
+				}, BOOK_DURATION);
+		}
+	});
 });
-
-app.post("/submit", (req, resp) => {
-
-})
-
-app.listen(80, () => console.log("Listening"));
--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
-	// const BACKEND_URL = "https://www.jiftoo.dev/story/api/";
-	const BACKEND_URL = "http://localhost/";
+	import {onDestroy, onMount} from "svelte";
 
-	let userInput = "Your piece of the story!";
+	// const BACKEND_URL = "ws://www.jiftoo.dev/story/ws";
+	const BACKEND_URL = "ws://192.168.0.154";
+
+	let userInput = "";
 	let wordsAfterLimit = -1;
 	const handleInput = (ev) => {
-		const filteredInput = ev.target.value.replaceAll(/[^A-Za-z0-9.,!?"\- ]/g, "");
+		const filteredInput = ev.target.value.replaceAll(/[^A-Za-z0-9.,!?":\- ]/g, "");
 		if (userInput.length >= maxCharacters && wordsAfterLimit === -1) {
 			wordsAfterLimit = userInput.split(" ").length;
 		}
@@ -16,7 +18,6 @@
 		if (wordsAfterLimit === -1) {
 			userInput = filteredInput;
 		} else if (filteredInput.split(" ").length >= wordsAfterLimit) {
-			console.log(123);
 			const filteredWordLimitedInput = filteredInput.split(" ").slice(0, wordsAfterLimit).join(" ");
 			userInput = filteredWordLimitedInput.substr(0, maxCharactersStretch);
 		}
@@ -28,39 +29,129 @@
 	$: counterColor = userInput.length === maxCharactersStretch ? "red" : "unset";
 	$: vibrate = userInput.length > maxCharacters ? "vibrate" : "";
 
-	const fetchPreviousEpisodes = async () => {
-		return await fetch(BACKEND_URL + "get", {
-			method: "GET",
-			cache: "no-cache",
-		}).then((r) => r.json());
+	let episodeArray = [];
+	let isBooked = false;
+	let bookedForMe = false;
+	let bookedFor = null;
+
+	let bookedUntilTimer = null;
+	let bookId = localStorage.getItem("st-bookid");
+	console.log("bookId", bookId);
+
+	let isGibberish = false;
+
+	let ws = null;
+	const connect = () => {
+		ws = new WebSocket(BACKEND_URL);
+		ws.addEventListener("message", (ev) => {
+			const msg = JSON.parse(ev.data);
+			switch (msg.type) {
+				case "update":
+					episodeArray = msg.data.episodes;
+					isBooked = msg.isBooked;
+                    bookedForMe = msg.data.bookedForMe;
+					bookedFor = msg.data.bookedFor;
+					clearInterval(bookedUntilTimer);
+					if (bookedFor > 0) {
+						bookedUntilTimer = setInterval(() => {
+							bookedFor -= 1000;
+						}, 1000);
+					}
+					break;
+				case "bookSuccess":
+					isBooked = true;
+					bookedForMe = true;
+					bookId = msg.data;
+					localStorage.setItem("st-bookid", bookId);
+					console.log("you booked", bookId);
+					break;
+				case "booked":
+					isBooked = true;
+					bookedFor = msg.data.bookedFor;
+					localStorage.setItem("st-booked-for", bookedFor.toString());
+					console.log("someone booked for", bookedFor);
+					bookedUntilTimer = setInterval(() => {
+						bookedFor -= 1000;
+					}, 1000);
+					break;
+				case "unbooked":
+					isBooked = false;
+					bookId = null;
+					bookedFor = 0;
+					clearInterval(bookedUntilTimer);
+					localStorage.setItem("st-booked-for", bookedFor.toString());
+					localStorage.setItem("st-bookid", "false");
+					console.log("unbooked");
+					break;
+				case "error":
+					isGibberish = msg.data === "gibberish";
+					console.error(msg.data);
+					break;
+			}
+		});
+		ws.addEventListener("open", () => {
+			ws.send(JSON.stringify({type: "get", bookId}));
+		});
+		ws.addEventListener("error", () => {
+			setTimeout(connect, 2500);
+		});
+	};
+	onMount(connect);
+
+	onDestroy(() => {
+		ws?.close();
+		clearInterval(bookedUntilTimer);
+	});
+
+	const handleSubmit = () => {
+		isGibberish = false;
+		ws.send(JSON.stringify({type: "submit", bookId, data: userInput}));
 	};
 
-    const submitEpisode = async () => {
-        
-    }
+	let showBookDialog = false;
+
+	const handleBook = () => {
+		showBookDialog = false;
+		ws.send(JSON.stringify({type: "book", bookId}));
+	};
 </script>
 
 <main>
+	<dialog open={showBookDialog}>
+		<p>Booking allows you to prevent other users from submitting their episodes for a short period of time.</p>
+		<center>
+			<button on:click={handleBook}>Book!</button>
+			<span>&nbsp;</span>
+			<button on:click={() => (showBookDialog = false)}>Cancel</button>
+		</center>
+	</dialog>
 	<center>
 		<h1>Storytelling experiment</h1>
 		<p>TODO: add description</p>
 		<br />
-		<form>
-			<blockquote>
-				{#await fetchPreviousEpisodes() then episodeArray}
-					{#each episodeArray as episode}
-						<cite>{episode}</cite>
-					{/each}
-				{/await}
-				<div>
-					<textarea type="text" bind:value={userInput} on:input={handleInput} />
-					<div id="submit-region">
-						<div id="counter" style="color: {counterColor}; animation-name: {vibrate}">{userInput.length} / {maxCharacters}</div>
-						<input type="submit" value="Submit!" />
-					</div>
+		<blockquote>
+			{#each episodeArray as episode}
+				<cite>{episode}</cite>
+			{/each}
+			<div>
+				<textarea placeholder="Your piece of the story!" type="text" bind:value={userInput} on:input={handleInput} />
+				{#if isGibberish}
+					<div>Submitted string is likely gibberish.</div>
+				{/if}
+				<div id="submit-region">
+					<div id="counter" style="color: {counterColor}; animation-name: {vibrate}">{userInput.length} / {maxCharacters}</div>
+					<button type="submit" disabled={isBooked && !bookedForMe} on:click={handleSubmit}>Submit!</button>
+					<button disabled={isBooked} on:click={() => (showBookDialog = true)}>Book</button>
+					{#if isBooked}
+						<div style="float: right">
+							Booked for: {((time) => {
+								return `${Math.floor(time / 1000 / 60)}  minutes ${Math.floor((time / 1000) % 60)} seconds`;
+							})(bookedFor)}
+						</div>
+					{/if}
 				</div>
-			</blockquote>
-		</form>
+			</div>
+		</blockquote>
 	</center>
 </main>
 
@@ -77,6 +168,8 @@
 
 		max-width: 50%;
 		text-align: left;
+
+		margin-bottom: 150px;
 	}
 	blockquote *:not(:first-child) {
 		margin-top: var(--padding);
@@ -101,6 +194,7 @@
 		height: calc(var(--lh) * 3rem + var(--pad) * 2);
 	}
 	cite {
+		display: block;
 		max-width: 100%;
 		border-left: 8px solid gainsboro;
 		padding-left: 8px;
@@ -109,8 +203,9 @@
 		display: inline-block;
 		margin-top: 0;
 	}
-	#submit-region input {
+	#submit-region button {
 		float: right;
+		margin-left: 8px;
 	}
 	#counter {
 		animation-duration: 100ms;
@@ -129,5 +224,10 @@
 		100% {
 			transform: translate(-0.7px, -0.7px);
 		}
+	}
+	dialog {
+		position: fixed;
+		top: 40%;
+		border: 1px solid rgb(32, 32, 32);
 	}
 </style>
